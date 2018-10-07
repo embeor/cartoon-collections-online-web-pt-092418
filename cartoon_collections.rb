dcc0169fe778fb151cd3920a88e1b466741e7298
Emilio Betancourt <embeor@gmail.com>
2018-10-07 18:46:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,6 @@ def summon_captain_planet (elements)# code an argument here
   elements.collect do |element|
     element.capitalize
   end
-  elements
 end
 
 def long_planeteer_calls (calls)# code an argument here
