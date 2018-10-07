18f0b796da8d0839e19e1c798eadaa02a7e17b9c
Emilio Betancourt <embeor@gmail.com>
2018-10-07 18:19:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def roll_call_dwarves (dwarves)# code an argument here
 end
 
 def summon_captain_planet (elements)# code an argument here
-  elements.map! {|element| element.capitalize}
+  elements.map! {|element| element.capitalize"!""}
 end
 
 def long_planeteer_calls (calls)# code an argument here
